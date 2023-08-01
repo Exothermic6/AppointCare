d7a5ac8f22f53fa9ae84f210ddf4101f72bd67d8
Rafsa Asman <aspharnamsat@gmail.com>
2023-08-01 09:06:37 +0300

    started creating the nav

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+export default function Nav() {
+    return (
+     <div>
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>Booking</li>
+                <li>Sign Up</li>
+                <li>Log In</li>
+                <button>Free Consultation</button>
+            </ul>
+        </div>
+     </div>
+    );
+  }
+  
