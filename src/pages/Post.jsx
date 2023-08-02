eb0f8990c6198b44ee522863db53d65edf85077f
Rafsa Asman <aspharnamsat@gmail.com>
2023-08-02 12:30:54 +0300

    changed the input's color and added outlines

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -9,36 +9,36 @@ export default function Post() {
         action=""
         className="container mx-auto flex flex-col shadow-xl w-[30vw] my-[3em] justify-center items-center"
       >
-        <div className="my-[2em]">
-          <label>Hospital's Image</label>
+        <div className="my-[1em]">
+          <label className="my-[1em] font-medium text-[1.5rem] text-left">Hospital's Image</label>
           <input
             type="file"
             name=""
             id=""
             placeholder="Add An Image Of The Hospital"
-            className="text-left bg-red  p-4  border-2 border-black flex outline-none"
+            className="text-left bg-red  p-4  border-2 border-gray-300 rounded-full flex outline-blue-400"
           />
         </div>
-        <div className="my-[2em]">
-          <label htmlFor="">Hospital's Name</label>
+        <div className="my-[1em]">
+          <label className="my-[1em] font-medium text-[1.5rem] text-left">Hospital's Name</label>
           <input
             type="text"
             placeholder="Add The Name Of The Hospital"
-            className="text-left bg-red  p-4  border-2 border-black flex outline-none"
+            className="text-left bg-red  p-4  border-2  border-gray-300 rounded-full flex outline-blue-400"
           />
         </div>
-        <div className="my-[2em]">
-          <label htmlFor="">Hospital's location</label>
+        <div className="my-[1em]">
+          <label className="my-[1em] font-medium text-[1.5rem] text-left">Hospital's location</label>
           <input
             type="text"
             name=""
             id=""
             placeholder="Add The Location Of The Hospital"
-            className="text-left bg-red  p-4  border-2 border-black flex outline-none"
+            className="text-left bg-red  p-4  border-2  border-gray-300 rounded-full flex outline-blue-500"
           />
         </div>
 
-        <button className="bg-[#3ba0f3] p-4 my-[1em] text-white rounded shadow">
+        <button className="bg-[#3ba0f3] p-4 my-[1em] rounded-full text-white shadow">
           Book Now
         </button>
       </form>
